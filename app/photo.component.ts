1d10424ef0ee95fd9c411a735fa73d57b6166670
Will Hancock <wwhancock@gmail.com>
2016-07-14 15:34:41 -0400

    updating to latest angular2 verions

---
+++ app/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Photo } from './photo';
+
+@Component({
+  selector: 'photo',
+  template: `
+  	<h1>My First Angular 2 App</h1>
+  	<img src="" />
+	`
+})
+export class PhotoComponent { }
